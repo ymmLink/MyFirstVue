937ed4fd431b8069490923094a96e1112b41fa33
ymmLink <237426636@qq.com>
2020-06-10 18:00:05 +0800

    构建VUE项目，配置Config文件，配置路由，添加elmentUI

---
+++ vue-admin/src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        我是谁
+    </div>
+</template>
+<script>
+export default {
+  name: "login"
+};
+</script>
+<style lang="less" scoped>
+#login{
+  height: 100vh;
+  background-color: aqua;
+}
+</style>
